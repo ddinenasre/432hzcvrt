33250c0cdbdd8057b80a83be1698d8c1083bbab8
ddinenasre <ddinenasre0@gmail.com>
2026-05-21 11:47:45 +0100

    Add files via upload

---
+++ file.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit - 432 Hz Audio Converter"
+git branch -M main
+git remote add origin https://github.com/USERNAME/REPO-NAME.git
+git push -u origin main
